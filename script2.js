6a2f7ff7d92b617794a57134904d013efe3e74c7
Francois Legillon <francois.legillon@contino.io@Francoiss-MacBook-Pro.local>
2023-09-27 08:25:47 +1000

    trying

---
+++ script2.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const getStartedButton = document.getElementById("getStartedButton");
+
+    getStartedButton.addEventListener("click", function () {
+        redirectToMusicPage();
+    });
+
+    function redirectToMusicPage() {
+        // You can replace this with the actual URL of your music page.
+        window.location.href = 'index2.html';
+    }
+});
